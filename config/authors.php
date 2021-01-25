a237805612439225e261a28c4caa2427e7347361
Sofia <sofia.perlari@gmail.com>
2021-01-25 12:20:26 +0100

    esempio carbon create form format

--- config/authors.php
+++ config/authors.php
@@ -4,21 +4,25 @@ return [
     [
         'name' => 'Mario',
         'lastname' => 'Rossi',
-        'date_of_birth' => '1990-01-01'
+        // 'date_of_birth' => '1990-01-01'
+        'date_of_birth' => '01/01/1990'
     ],
     [
         'name' => 'Luca',
         'lastname' => 'Verdi',
-        'date_of_birth' => '1992-04-01'
+        // 'date_of_birth' => '1992-04-01'
+        'date_of_birth' => '01/04/1992'
     ],
     [
         'name' => 'Silvia',
         'lastname' => 'Neri',
-        'date_of_birth' => '1991-09-01'
+        // 'date_of_birth' => '1991-09-01'
+        'date_of_birth' => '01/09/1991'
     ],
     [
         'name' => 'Giuseppe',
         'lastname' => 'Bianchi',
-        'date_of_birth' => '1992-01-30'
+        // 'date_of_birth' => '1992-01-30'
+        'date_of_birth' => '30/01/1992'
     ]
 ];
--- database/seeds/AuthorsTableSeeder.php
+++ database/seeds/AuthorsTableSeeder.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Seeder;
 use App\Author;
 use Faker\Generator as Faker;
+use Carbon\Carbon;
 
 class AuthorsTableSeeder extends Seeder
 {
@@ -13,23 +14,24 @@ class AuthorsTableSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
-        // $authors = config('authors');
-        //
-        // foreach ($authors as $author) {
-        //     $new_author_object = new Author();
-        //     $new_author_object->name = $author['name'];
-        //     $new_author_object->lastname = $author['lastname'];
-        //     $new_author_object->date_of_birth = $author['date_of_birth'];
-        //     $new_author_object->save();
-        // }
+        $authors = config('authors');
 
-        for ($i=0; $i < 100; $i++) {
+        foreach ($authors as $author) {
             $new_author_object = new Author();
-            $new_author_object->name = $faker->firstName();
-            $new_author_object->lastname = $faker->lastName();
-            $new_author_object->date_of_birth = $faker->date();
+            $new_author_object->name = $author['name'];
+            $new_author_object->lastname = $author['lastname'];
+            $data_nascita = $author['date_of_birth'];
+            $new_author_object->date_of_birth = Carbon::createFromFormat('d/m/Y', $data_nascita);
             $new_author_object->save();
         }
+
+        // for ($i=0; $i < 100; $i++) {
+        //     $new_author_object = new Author();
+        //     $new_author_object->name = $faker->firstName();
+        //     $new_author_object->lastname = $faker->lastName();
+        //     $new_author_object->date_of_birth = $faker->date();
+        //     $new_author_object->save();
+        // }
 
     }
 }
